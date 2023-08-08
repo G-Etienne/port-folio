44a070faee506aa649e9b496707006946913d5bd
G-etienne <ginetetienne2@gmail.com>
2023-08-08 09:46:41 -0400

    the lamp on the desk is building and the style is adding to it.

--- src/components/desk/Light.jsx
+++ src/components/desk/Light.jsx
@@ -6,24 +6,28 @@ import { ThemeContext } from "../../contexts/ThemeContext";
 //---------------------------------------------------------
 //style
 import "../../styles/components/desk/_light.scss";
+import "../../styles/components/desk/desk-media/_lightMediaQueries.scss";
 
 //---------------------------------------------------------
 function Light() {
+    //context for the theme
     const { theme } = useContext(ThemeContext);
 
     return (
         <>
             <div className="light">
+                {/* top of the light */}
                 <div className={`light__head__top ${theme + "HeadTop"}`}></div>
                 <div className={`light__head ${theme + "Head"}`}></div>
                 <div className={`light__on__top ${theme + "Head"}`}></div>
                 <div className={`light__on ${theme + "Head"}`}></div>
                 <div className={`light__on__bottom ${theme + "Head"}`}></div>
-                <div></div>
                 <div className={`light__arm ${theme + "Arm"}`}></div>
+                {/* arm of the light */}
                 <div
                     className={`light__arm__Shadow ${theme + "ArmShadow"}`}
                 ></div>
+                {/* base of the light */}
                 <div className={`light__baseTop ${theme + "BaseTop"}`}></div>
                 <div
                     className={`light__baseMiddle ${theme + "BaseMiddle"}`}
