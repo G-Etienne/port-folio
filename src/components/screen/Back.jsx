69d7197112965849be77881beebb2bfb56d950ce
G-etienne <ginetetienne2@gmail.com>
2023-08-11 15:57:32 -0400

    back screen animation without comments

--- src/components/screen/Back.jsx
+++ src/components/screen/Back.jsx
@@ -14,15 +14,79 @@ function Back() {
         <>
             <div className={`backSide ${theme}`}></div>
             <div className={`backAnim ${theme}`}>
-                <div className={`dot__one ${theme}`}>
-                    <div className={`dot__ancor__one ${theme}`}>
+                <div className="backAnim__container">
+                    <div className={`dot__one ${theme}`}>
+                        <div className={`dot__ancor__one ${theme}`}>
+                            <div className={`rod__one ${theme}`}>
+                                <div className={`dot__two ${theme}`}>
+                                    <div className={`rod__two ${theme}`}>
+                                        <div
+                                            className={`specialRod__one ${theme}`}
+                                        ></div>
+                                        <div className={`dot__three ${theme}`}>
+                                            <div
+                                                className={`rod__three ${theme}`}
+                                            >
+                                                <div
+                                                    className={`specialRod__two ${theme}`}
+                                                ></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={`dot__ancor__two ${theme}`}>
+                            <div className={`rod__one ${theme}`}>
+                                <div className={`dot__two ${theme}`}>
+                                    <div className={`rod__two ${theme}`}>
+                                        <div
+                                            className={`specialRod__three ${theme}`}
+                                        ></div>
+                                        <div className={`dot__three ${theme}`}>
+                                            <div
+                                                className={`rod__three ${theme}`}
+                                            >
+                                                <div
+                                                    className={`specialRod__four ${theme}`}
+                                                ></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={`dot__ancor__three ${theme}`}>
+                            <div className={`rod__one ${theme}`}>
+                                <div className={`dot__two ${theme}`}>
+                                    <div className={`rod__two ${theme}`}>
+                                        <div
+                                            className={`specialRod__five ${theme}`}
+                                        ></div>
+
+                                        <div className={`dot__three ${theme}`}>
+                                            <div
+                                                className={`rod__three ${theme}`}
+                                            >
+                                                <div
+                                                    className={`specialRod__six ${theme}`}
+                                                ></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                         <div className={`rod__one ${theme}`}>
                             <div className={`dot__two ${theme}`}>
                                 <div className={`rod__two ${theme}`}>
+                                    <div
+                                        className={`specialRod__seven ${theme}`}
+                                    ></div>
                                     <div className={`dot__three ${theme}`}>
                                         <div className={`rod__three ${theme}`}>
                                             <div
-                                                className={`specialRod__one ${theme}`}
+                                                className={`specialRod__eight ${theme}`}
                                             ></div>
                                         </div>
                                     </div>
@@ -30,39 +94,81 @@ function Back() {
                             </div>
                         </div>
                     </div>
-                    <div className={`dot__ancor__two ${theme}`}>
-                        <div className={`rod__one ${theme}`}>
-                            <div className={`dot__two ${theme}`}>
-                                <div className={`rod__two ${theme}`}>
-                                    <div className={`dot__three ${theme}`}>
+                    <div className={`dot__one__bis ${theme}`}>
+                        <div className={`dot__ancor__one ${theme}`}>
+                            <div className={`rod__one ${theme}`}>
+                                <div className={`dot__two ${theme}`}>
+                                    <div className={`rod__two ${theme}`}>
                                         <div
-                                            className={`rod__three ${theme}`}
+                                            className={`specialRod__one ${theme}`}
                                         ></div>
+                                        <div className={`dot__three ${theme}`}>
+                                            <div
+                                                className={`rod__three ${theme}`}
+                                            >
+                                                <div
+                                                    className={`specialRod__two ${theme}`}
+                                                ></div>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div className={`dot__ancor__three ${theme}`}>
-                        <div className={`rod__one ${theme}`}>
-                            <div className={`dot__two ${theme}`}>
-                                <div className={`rod__two ${theme}`}>
-                                    <div className={`dot__three ${theme}`}>
+                        <div className={`dot__ancor__two ${theme}`}>
+                            <div className={`rod__one ${theme}`}>
+                                <div className={`dot__two ${theme}`}>
+                                    <div className={`rod__two ${theme}`}>
                                         <div
-                                            className={`rod__three ${theme}`}
+                                            className={`specialRod__three ${theme}`}
                                         ></div>
+                                        <div className={`dot__three ${theme}`}>
+                                            <div
+                                                className={`rod__three ${theme}`}
+                                            >
+                                                <div
+                                                    className={`specialRod__four ${theme}`}
+                                                ></div>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div className={`rod__one ${theme}`}>
-                        <div className={`dot__two ${theme}`}>
-                            <div className={`rod__two ${theme}`}>
-                                <div className={`dot__three ${theme}`}>
+                        <div className={`dot__ancor__three ${theme}`}>
+                            <div className={`rod__one ${theme}`}>
+                                <div className={`dot__two ${theme}`}>
+                                    <div className={`rod__two ${theme}`}>
+                                        <div
+                                            className={`specialRod__five ${theme}`}
+                                        ></div>
+
+                                        <div className={`dot__three ${theme}`}>
+                                            <div
+                                                className={`rod__three ${theme}`}
+                                            >
+                                                <div
+                                                    className={`specialRod__six ${theme}`}
+                                                ></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={`rod__one ${theme}`}>
+                            <div className={`dot__two ${theme}`}>
+                                <div className={`rod__two ${theme}`}>
                                     <div
-                                        className={`rod__three ${theme}`}
+                                        className={`specialRod__seven ${theme}`}
                                     ></div>
+                                    <div className={`dot__three ${theme}`}>
+                                        <div className={`rod__three ${theme}`}>
+                                            <div
+                                                className={`specialRod__eight ${theme}`}
+                                            ></div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
