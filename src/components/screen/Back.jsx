64f3a30e8d7aaaea1b4a55ef0c66ccd16143a762
G-etienne <ginetetienne2@gmail.com>
2023-08-14 10:03:49 -0400

    end building animation of the back

--- src/components/screen/Back.jsx
+++ src/components/screen/Back.jsx
@@ -4,177 +4,26 @@ import { useContext } from "react";
 import { ThemeContext } from "../../contexts/ThemeContext";
 
 //--------------------------------------------------
+//style
 import "../../styles/components/screen/_back.scss";
 
 //--------------------------------------------------
+//animation element
+import AnimBack from "./AnimBack";
+
+//--------------------------------------------------
 function Back() {
+    {
+        /* themes */
+    }
     const { theme } = useContext(ThemeContext);
 
     return (
         <>
+            {/* container for the back which cover the animation */}
             <div className={`backSide ${theme}`}></div>
-            <div className={`backAnim ${theme}`}>
-                <div className="backAnim__container">
-                    <div className={`dot__one ${theme}`}>
-                        <div className={`dot__ancor__one ${theme}`}>
-                            <div className={`rod__one ${theme}`}>
-                                <div className={`dot__two ${theme}`}>
-                                    <div className={`rod__two ${theme}`}>
-                                        <div
-                                            className={`specialRod__one ${theme}`}
-                                        ></div>
-                                        <div className={`dot__three ${theme}`}>
-                                            <div
-                                                className={`rod__three ${theme}`}
-                                            >
-                                                <div
-                                                    className={`specialRod__two ${theme}`}
-                                                ></div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className={`dot__ancor__two ${theme}`}>
-                            <div className={`rod__one ${theme}`}>
-                                <div className={`dot__two ${theme}`}>
-                                    <div className={`rod__two ${theme}`}>
-                                        <div
-                                            className={`specialRod__three ${theme}`}
-                                        ></div>
-                                        <div className={`dot__three ${theme}`}>
-                                            <div
-                                                className={`rod__three ${theme}`}
-                                            >
-                                                <div
-                                                    className={`specialRod__four ${theme}`}
-                                                ></div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className={`dot__ancor__three ${theme}`}>
-                            <div className={`rod__one ${theme}`}>
-                                <div className={`dot__two ${theme}`}>
-                                    <div className={`rod__two ${theme}`}>
-                                        <div
-                                            className={`specialRod__five ${theme}`}
-                                        ></div>
-
-                                        <div className={`dot__three ${theme}`}>
-                                            <div
-                                                className={`rod__three ${theme}`}
-                                            >
-                                                <div
-                                                    className={`specialRod__six ${theme}`}
-                                                ></div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className={`rod__one ${theme}`}>
-                            <div className={`dot__two ${theme}`}>
-                                <div className={`rod__two ${theme}`}>
-                                    <div
-                                        className={`specialRod__seven ${theme}`}
-                                    ></div>
-                                    <div className={`dot__three ${theme}`}>
-                                        <div className={`rod__three ${theme}`}>
-                                            <div
-                                                className={`specialRod__eight ${theme}`}
-                                            ></div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className={`dot__one__bis ${theme}`}>
-                        <div className={`dot__ancor__one ${theme}`}>
-                            <div className={`rod__one ${theme}`}>
-                                <div className={`dot__two ${theme}`}>
-                                    <div className={`rod__two ${theme}`}>
-                                        <div
-                                            className={`specialRod__one ${theme}`}
-                                        ></div>
-                                        <div className={`dot__three ${theme}`}>
-                                            <div
-                                                className={`rod__three ${theme}`}
-                                            >
-                                                <div
-                                                    className={`specialRod__two ${theme}`}
-                                                ></div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className={`dot__ancor__two ${theme}`}>
-                            <div className={`rod__one ${theme}`}>
-                                <div className={`dot__two ${theme}`}>
-                                    <div className={`rod__two ${theme}`}>
-                                        <div
-                                            className={`specialRod__three ${theme}`}
-                                        ></div>
-                                        <div className={`dot__three ${theme}`}>
-                                            <div
-                                                className={`rod__three ${theme}`}
-                                            >
-                                                <div
-                                                    className={`specialRod__four ${theme}`}
-                                                ></div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className={`dot__ancor__three ${theme}`}>
-                            <div className={`rod__one ${theme}`}>
-                                <div className={`dot__two ${theme}`}>
-                                    <div className={`rod__two ${theme}`}>
-                                        <div
-                                            className={`specialRod__five ${theme}`}
-                                        ></div>
-
-                                        <div className={`dot__three ${theme}`}>
-                                            <div
-                                                className={`rod__three ${theme}`}
-                                            >
-                                                <div
-                                                    className={`specialRod__six ${theme}`}
-                                                ></div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className={`rod__one ${theme}`}>
-                            <div className={`dot__two ${theme}`}>
-                                <div className={`rod__two ${theme}`}>
-                                    <div
-                                        className={`specialRod__seven ${theme}`}
-                                    ></div>
-                                    <div className={`dot__three ${theme}`}>
-                                        <div className={`rod__three ${theme}`}>
-                                            <div
-                                                className={`specialRod__eight ${theme}`}
-                                            ></div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            {/* animation */}
+            <AnimBack />
         </>
     );
 }
